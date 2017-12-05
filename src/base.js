7e819ca9e74be8f7a17be05ae57a281c20cf7ea3
Justin Bond <justin.b@530medialab.com>
2017-12-04 16:37:08 -0800

    Linked Firebase for persisting our State

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyABCiwcQ-77j6_MfMe1TNVjVE_nX-KyXl0",
+    authDomain: "catch-of-the-day-justin-bond.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-justin-bond.firebaseio.com",
+});
+
+export default base;
