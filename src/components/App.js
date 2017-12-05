a92b60d5fd0ddee907981bf87a7b14709a6da5b8
Justin Bond <justin.b@530medialab.com>
2017-12-05 09:24:51 -0800

    render the inventory, on change for inventory

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.addFish = this.addFish.bind(this);
+		this.updateFish = this.updateFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
 		// getInitailState
@@ -57,6 +58,12 @@ class App extends React.Component {
 		this.setState({ fishes });
 	}
 
+	updateFish(key, updatedFish) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({ fishes });
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -85,8 +92,17 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order} params={this.props.params} />
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+				<Order 
+					fishes={this.state.fishes}
+					order={this.state.order}
+					params={this.props.params}
+				/>
+				<Inventory
+					fishes={this.state.fishes} 
+					addFish={this.addFish}
+					loadSamples={this.loadSamples} 
+					updateFish={this.updateFish} 
+				/>
 			</div>
 		);
 	}
